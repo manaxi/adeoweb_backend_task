dbb82b0cd6cf30624d80ca3d8fe3049c57c2b898
manaxi <manaxi17@gmail.com>
2020-07-14 18:55:18 +0300

    Added Models

---
+++ app/Product.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    // Table name
+    protected $table = 'products';
+    // Primary key
+    public $primaryKey = 'id';
+    //Timestamps
+    public $timestamps = true;
+
+    protected $fillable = ['name', 'sku', 'price', 'status'];
+}
